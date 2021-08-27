3b7e59f9ed82f64b35c1232db88c7b1e27328d94
Lucas Coronel <lccoronel7@icloud.com>
2021-08-27 15:15:55 -0300

    feat: list all users controller

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -5,7 +5,17 @@ import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {}
+  handle(request: Request, response: Response): Response {
+    try {
+      const { user_id } = request.params;
+
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
 }
 
 export { ListAllUsersController };
